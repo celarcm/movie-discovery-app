0fccbd99d950aa96838266fa5c5402caf09e718c
Matej Celarc <matejcelarc@gmail.com>
2026-02-16 00:08:13 +0100

    add function to fetch popular movies and display them on the homepage

---
+++ lib/tmdb.ts
@@ -0,0 +1,14 @@
+export const getPopularMovies = async (page: number = 1) => {
+    const API_KEY = process.env.TMDB_API_KEY;
+    const BASE_URL = 'https://api.themoviedb.org/3';
+
+    const response = await fetch(
+        `${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${page}`,
+    );
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch popular movies");
+    }
+
+    return response.json();
+};
