87f2b5bfbdf76148123907ac735ca43423ca1064
Matej Celarc <matejcelarc@gmail.com>
2026-02-16 00:33:01 +0100

    add a check to see if TMDB API key is set and throw an error when its not

--- lib/tmdb.ts
+++ lib/tmdb.ts
@@ -1,9 +1,13 @@
 import { TMDBResponse } from "@/types/movie";
 
-export const getPopularMovies = async (page: number = 1): Promise<TMDBResponse> => {
-    const API_KEY = process.env.TMDB_API_KEY;
-    const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = process.env.TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
 
+if (!API_KEY) {
+    console.warn("TMDB API key is not set. Please add TMDB_API_KEY to your .env file");
+}
+
+export const getPopularMovies = async (page: number = 1): Promise<TMDBResponse> => {
     const response = await fetch(
         `${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${page}`,
     );
