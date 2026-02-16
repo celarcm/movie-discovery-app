04b1d0c843f3aba232516e4aabb26eae9bacd3af
Matej Celarc <matejcelarc@gmail.com>
2026-02-16 01:18:11 +0100

    edit grid layout to show move movies in a row for each screen size

--- components/MovieGrid.tsx
+++ components/MovieGrid.tsx
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 
 export default function MovieGrid({ movies }: { movies: Movie[] }) {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {movies.map((movie: Movie) => (
                 <MovieCard key={movie.id} movie={movie} />
             ))}
