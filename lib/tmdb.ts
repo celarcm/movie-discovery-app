d2847c75c73a87aeb14b188c2f66374054e0dde7
Matej Celarc <matejcelarc@gmail.com>
2026-02-17 00:34:43 +0100

    add api call to get movie details, add function to generate image url

--- lib/tmdb.ts
+++ lib/tmdb.ts
@@ -1,11 +1,16 @@
-import { TMDBResponse } from "@/types/movie";
+import { TMDBResponse, MovieDetails } from "@/types/movie";
 
 const API_KEY = process.env.TMDB_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
 
 if (!API_KEY) {
     console.warn("TMDB API key is not set. Please add TMDB_API_KEY to your .env file");
 }
+
+export const getImageUrl = (path: string | null, size: string = "w500"): string => {
+    return `${IMAGE_BASE_URL}/${size}${path}`;
+};
 
 export const getPopularMovies = async (page: number = 1): Promise<TMDBResponse> => {
     const response = await fetch(
@@ -30,6 +35,18 @@ export const searchMovies = async (query: string, page: number = 1): Promise<TMD
 
     if (!response.ok) {
         throw new Error("Failed to search movies");
+    }
+
+    return response.json();
+};
+
+export const getMovieDetails = async (id: number): Promise<MovieDetails> => {
+    const response = await fetch(
+        `${BASE_URL}/movie/${id}?api_key=${API_KEY}`,
+    );
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch movie details");
     }
 
     return response.json();
--- types/movie.ts
+++ types/movie.ts
@@ -15,6 +15,43 @@ export interface Movie {
     video: boolean;
 }
 
+export interface MovieDetails extends Movie {
+    genres: Genre[];
+    runtime: number;
+    budget: number;
+    revenue: number;
+    status: string;
+    tagline: string;
+    homepage: string;
+    production_companies: ProductionCompany[];
+    production_countries: ProductionCountry[];
+    spoken_languages: SpokenLanguage[];
+}
+
+export interface Genre {
+    id: number;
+    name: string;
+}
+
+export interface ProductionCompany {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+}
+
+export interface ProductionCountry {
+    iso_3166_1: string;
+    name: string;
+}
+
+export interface SpokenLanguage {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+}
+
+
 export interface TMDBResponse {
     page: number;
     results: Movie[];
