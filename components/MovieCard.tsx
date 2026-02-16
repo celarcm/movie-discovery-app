8e2f3d8fe6fe38d0219dc958202f84897e50261e
Matej Celarc <matejcelarc@gmail.com>
2026-02-16 01:18:52 +0100

    add scale effect on hover for MovieCard

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 
 export default function MovieCard({ movie }: { movie: Movie }) {
     return (
-        <div key={movie.id}>
-            <h2>{movie.title}</h2>
+        <div key={movie.id} className="transition-transform duration-300 hover:scale-105">
             <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} width={500} height={500} />
         </div>
     )
