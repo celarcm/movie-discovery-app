aab0f44219e37e876bda80796c4fb17b1476f15e
Matej Celarc <matejcelarc@gmail.com>
2026-02-16 23:28:31 +0100

    handle case when no movies are returned from api

--- components/MovieGrid.tsx
+++ components/MovieGrid.tsx
@@ -1,7 +1,24 @@
 import { Movie } from "@/types/movie";
 import MovieCard from "./MovieCard";
 
-export default function MovieGrid({ movies }: { movies: Movie[] }) {
+interface MovieGridProps {
+    movies: Movie[];
+    searchQuery?: string;
+}
+
+export default function MovieGrid({ movies, searchQuery }: MovieGridProps) {
+    if (movies.length === 0) {
+        return (
+            <div className="text-center py-16">
+                <p className="text-xl text-gray-400">
+                    {searchQuery
+                        ? `No movies found for "${searchQuery}"`
+                        : "No movies available"}
+                </p>
+            </div>
+        );
+    }
+
     return (
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {movies.map((movie: Movie) => (
