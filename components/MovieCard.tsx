906eb323cea0ca562d630df85668b6f4af17152d
Matej Celarc <matejcelarc@gmail.com>
2026-02-16 23:37:38 +0100

    add fallback for when movie poster is not present, update styling

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -4,7 +4,15 @@ import Image from "next/image";
 export default function MovieCard({ movie }: { movie: Movie }) {
     return (
         <div key={movie.id} className="transition-transform duration-300 hover:scale-105">
-            <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} width={500} height={500} />
+            {movie.poster_path ? (
+                <Image className="rounded-md" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} width={500} height={500} />
+            ) : (
+                <div className="w-full h-full flex items-center justify-center rounded-md border border-gray-500 bg-black">
+                    <div className="text-center text-gray-200 font-medium px-2">{movie.title}</div>
+                </div>
+            )
+            }
+
         </div>
     )
 }
